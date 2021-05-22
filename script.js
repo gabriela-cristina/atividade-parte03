c6096469a8ff7423befc57b4bf448c64b1827c48
Gabriela <80904845+gabriela-cristina@users.noreply.github.com>
2021-05-22 12:59:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let a = parseInt(prompt ('Digite aqui o primeiro número!'));
+
+let b = parseInt(prompt ('Digite aqui o segundo número'));
+
+let c = parseInt(prompt ('Digite aqui o terceiro número'));
+
+let d = parseInt(prompt ('Digite aqui o quarto número'));
+
+let soma = (a) + (b) + (c) + (d);
+
+let media = soma / 4;
+
+alert( `A soma é: `  + soma + ` e a media é: ` + media + `!` )
+
+
+
